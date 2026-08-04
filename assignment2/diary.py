1a18036758494a37ea2396eff1e7b666ddefbbc8
Jennifer Hoang <257028963+jenferno@users.noreply.github.com>
2026-08-04 03:15:39 -0500

    Completed Python assignment 2

---
+++ assignment2/diary.py
@@ -0,0 +1,33 @@
+## Task 1 : Diary
+
+import traceback
+
+try:
+    with open("diary.txt", "a") as file:
+        prompt = "What happened today? "
+
+        while True:
+            entry = input(prompt)
+            file.write(entry + "\n")
+
+            if entry == "done for now":
+                break
+
+            prompt = "What else? "
+
+except Exception as e:
+    trace_back = traceback.extract_tb(e.__traceback__)
+    stack_trace = []
+
+    for trace in trace_back:
+        stack_trace.append(
+            f"File : {trace[0]} , Line : {trace[1]}, Func.Name : {trace[2]}, Message : {trace[3]}"
+        )
+
+    print(f"Exception type: {type(e).__name__}")
+
+    message = str(e)
+    if message:
+        print(f"Exception message: {message}")
+
+    print(f"Stack trace: {stack_trace}")
